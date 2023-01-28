77a8b9a8dfb1cd442791154d75ce9dcf0d417dbb
bilyal1993 <bilyal79788635969b@gmail.com>
2023-01-28 13:49:36 +0300

    уменьшен размер

--- kvadr_ur.py
+++ kvadr_ur.py
@@ -7,14 +7,13 @@ print('Введите коэффициент c:')
 c=int(input())
 print('Решаем уравнение^ ', a,'x^2+',b,'x+',c,'=0')
 D=b*b-4*a*c
-if D<0: print('Корней нет')
-if D==0: print('Один корень')
-if D>0: print('Два корня')
-
 if D>0: 
+    print('Два корня')
     x1= ((-b)+ math.sqrt(D))/(2*a)
     x2= ((-b)- math.sqrt(D))/(2*a)
     print('x1=',x1,' x2=',x2)
 elif D==0:
+    print('Один корень')
     x=(-b)/(2*a)
     print('x=',x)
+else: print('Корней нет')
