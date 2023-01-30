a777ddb5fd8f72119430a5e86ae60d85daf68b5d
bilyal1993 <bilyal79788635969b@gmail.com>
2023-01-30 13:17:11 +0300

    улучшеное форматирование

--- radius.py
+++ radius.py
@@ -1,7 +1,10 @@
 import math
 print('Введите радиус круга:')
 r=int(input())
-S=math.pi*r*r
-print('Площадь круга равна ',S)
-l=math.pi*2*r
-print('Длина окружности ',l)
+if r<=0:
+    print('error')
+elif r>0:    
+    S=math.pi*r*r
+    print('Площадь круга равна ',"%4.2f"%S)
+    l=math.pi*2*r
+    print('Длина окружности ',"%4.2f"%l)
