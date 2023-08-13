a32a5c51c29e38a32744511fe8715829af693c08
bilyal1993 <bilyal79788635969b@gmail.com>
2023-08-13 16:52:23 +0300

    новые

--- hello.py
+++ hello.py
@@ -1,4 +1,12 @@
-print("введи число")
-a=int(input())
-a=a**a
-print(a)
+#def factorial(n):
+     #   if n==1: return 1
+       # return n*factorial(n-1)
+    
+#print(factorial(10))  
+def is_empty(string):
+        if len(string)==0: 
+                return True
+        else: 
+                return False
+
+print(is_empty(" ad"))
